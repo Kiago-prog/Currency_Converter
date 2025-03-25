9e0ee1f19d0d7109cf24a7661c9417745574045f
kiago <roy.kiago@student.moringaschool.com>
2025-03-25 11:34:20 +0300

    debug due to api failiur

---
+++ apiconfig.js
@@ -0,0 +1,1 @@
+const API_KEY = "cur_live_vve40AbFqZ19EVNIQbc137vIeDTwzwKb1ut8BbM8";
--- functions.js
+++ functions.js
@@ -1,18 +1,9 @@
 const populate=async(CSSMathValue, currency) => {
     let myStr=""
-    URL="https://api.currencyapi.com/v3/latest?apikey=https://api.currencyapi.com/v3/latest?apikey=cur_live_vve40AbFqZ19EVNIQbc137vIeDTwzwKb1ut8BbM8&base_currency="+currency
-    let response=await fetch(URL)
-    let data=await response.json()
-    document.querySelector(".output").computedStyleMap.display="block"
 
-    for (let key of Object.keys(data[data])){
-        myStr+=
-    `<tr>
-         <td>${key}</td>
-         <td>${data[data][key]["code"]}</td>
-         <td>${Math.round(data["data"][key]["value"]*value)}</td>
-     </tr>`
-    }
-    const tableBody = document.querySelector("tbody");
-    tableBody.innerHTML = myStr;
-}
+const apiUrl = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error("Error:", error));
