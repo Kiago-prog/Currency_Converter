55bb091ff44525cb26285d83526a3b9ed20ba5fa
kiago <roy.kiago@student.moringaschool.com>
2025-03-27 13:13:17 +0300

    Exchanger

--- js/script.js
+++ js/script.js
@@ -45,3 +45,23 @@ exchangeIcon.addEventListener("click", ()=>{
     loadFlag(toCurrency); 
     getExchangeRate(); 
 });
+
+function getExchangeRate(){
+  const amount = document.querySelector("form input");
+  const exchangeRateTxt = document.querySelector("form .exchange-rate");
+  let amountVal = amount.value;
+  // if user don't enter any value or enter 0 then we'll put 1 value by default in the input field
+  if(amountVal == "" || amountVal == "0"){
+      amount.value = "1";
+      amountVal = 1;
+  }
+  exchangeRateTxt.innerText = "Getting exchange rate...";
+  let url = `https://v6.exchangerate-api.com/v6/YOUR_API_KEY/latest/${fromCurrency.value}`;
+  fetch(url).then(response => response.json()).then(result =>{
+      let exchangeRate = result.conversion_rates[toCurrency.value]; // getting user selected TO currency rate
+      let totalExRate = (amountVal * exchangeRate).toFixed(2); // multiplying user entered value with selected TO currency rate
+      exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+  }).catch(() =>{ // if user is offline or any other error occured while fetching data then catch function will run
+      exchangeRateTxt.innerText = "Something went wrong";
+  });
+}
